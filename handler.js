10e404e84c97a94bbe3cea46c3f64aec7bd6c2f7
Raymond Yang <raymond.yang@centrapay.com>
2021-06-02 00:21:31 +1200

    Add cors middleware

--- handler.js
+++ handler.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require('cors');
+
 const items = require('./src/routes/items');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', items);
 
